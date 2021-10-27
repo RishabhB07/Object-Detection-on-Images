92905137dde8ee598f70bd0eec330d51c0f714d8
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-10-27 19:10:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+objects=[];
 function setup(){
     canvas=createCanvas(640, 420);
     canvas.center();
@@ -11,6 +12,17 @@ function preload(){
 }
 function draw(){
     image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            fill("#00FF00");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#00FF00");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        } 
+     }
     fill("#00FF00");
     text("Banana", 250, 30);
     noFill();
@@ -38,5 +50,6 @@ function gotResult(error, results){
     }
     else{
         console.log(results);
+        objects=results;
     }
 }
